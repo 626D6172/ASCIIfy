7b5eacdb591a52e7e1c5359a1ba03aabf3f2ddfa
Brandon <github@626d6172.com>
2025-06-30 18:20:28 -0500

    Using buffer instead of printing to screen

--- pkg/asciify/asciify.go
+++ pkg/asciify/asciify.go
@@ -2,34 +2,38 @@
 package asciify
 
 import (
-	"fmt"
 	"image"
 	"io"
 	"math"
 	"os"
+	"time"
 
 	_ "image/jpeg"
 	_ "image/png"
 
+	"github.com/gosuri/uilive"
 	"github.com/nfnt/resize"
 	"golang.org/x/term"
 )
 
 const SHADER = " .:coPO?@#"
 
-func ResizeImage(m image.Image, w uint, h uint) image.Image {
-	bounds := m.Bounds()
-	imageWidth := bounds.Max.X
-	imageHeight := bounds.Max.Y
-
-	var newImage image.Image
-	if imageWidth > imageHeight {
-		newImage = resize.Resize(w, 0, m, resize.Lanczos3)
-	} else {
-		newImage = resize.Resize(0, h, m, resize.Lanczos3)
+func CreateScreenBuffer(w int, h int) []byte {
+	buf := make([]byte, h*(w+1))
+	for i := range buf {
+		buf[i] = ' '
 	}
 
-	return newImage
+	// line endings
+	for i := 1; i <= h; i++ {
+		buf[((w+1)*i)-1] = '\n'
+	}
+
+	return buf
+}
+
+func ResizeImage(m image.Image, w uint, h uint) image.Image {
+	return resize.Thumbnail(w, h, m, resize.Lanczos3)
 }
 
 func SoftASCII(r io.Reader) error {
@@ -43,30 +47,35 @@ func SoftASCII(r io.Reader) error {
 		return err
 	}
 
-	m := ResizeImage(img, uint(w/2), uint(h))
+	img = ResizeImage(img, uint(w/2), uint(h))
 
-	bounds := m.Bounds()
-	width := bounds.Max.X
-	height := bounds.Max.Y
-
-	fmt.Println(width, height, w, h)
+	bounds := img.Bounds()
+	buf := CreateScreenBuffer(w, h)
+	screen := uilive.New()
+	screen.RefreshInterval = time.Millisecond * 200
+	screen.Start()
 
 	for y := range h {
-		for x := range w {
-			if x >= width {
+		for x := 0; x < w; x += 2 {
+			c := img.At(x/2, y)
+			if x/2 > bounds.Max.X {
+				buf[(y*w)+x] = SHADER[0]
+				buf[(y*w)+x+1] = SHADER[0]
 				continue
 			}
-			c := m.At(x, y)
 			r, g, b, _ := c.RGBA()
 			// Calc lum https://en.wikipedia.org/wiki/Relative_luminance
 			rf := 0.2126 * float64(r)
 			gf := 0.7152 * float64(g)
 			bf := 0.0722 * float64(b)
 			lum := int(math.Floor(((rf + gf + bf) / 0xffff) * float64(len(SHADER)-1)))
-			fmt.Print(string(SHADER[lum]), string(SHADER[lum]))
+			buf[(y*w)+x] = SHADER[lum]
+			buf[(y*w)+x+1] = SHADER[lum]
 		}
-		fmt.Print("\n")
 	}
+
+	screen.Write(buf)
+	screen.Stop()
 
 	return nil
 }
